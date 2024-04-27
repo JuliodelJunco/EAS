411c5676321384b41dbe04f15b55c6b0e44ae82e
JuliodelJunco <117302342+JuliodelJunco@users.noreply.github.com>
2024-04-27 19:33:10 +0200

    Add files via upload

---
+++ src/main/java/io/bit3/mgpm/worker/AbstractWorkerObserver.java
@@ -0,0 +1,21 @@
+package io.bit3.mgpm.worker;
+
+public abstract class AbstractWorkerObserver implements WorkerObserver {
+  @Override
+  public void start(Worker worker) {
+    // no op
+  }
+
+
+  //jdjjjjdjdjdjdj
+
+  @Override
+  public void activity(Activity activity, Worker worker) {
+    // no op
+  }
+
+  @Override
+  public void end(Worker worker) {
+    // no op
+  }
+}
