c7086ba016c75cb622bd903aa66e510188a1e8b7
JuliodelJunco <117302342+JuliodelJunco@users.noreply.github.com>
2024-04-29 18:41:11 +0200

    Update MainWindow.java

--- src/main/java/io/bit3/mgpm/gui/MainWindow.java
+++ src/main/java/io/bit3/mgpm/gui/MainWindow.java
@@ -1,8 +1,10 @@
 package io.bit3.mgpm.gui;
 
+import jdk.jfr.internal.Repository;
+
 import javax.swing.*;
 
 public class MainWindow {
-  private JList repositoryList;
+  private JList<Repository> repositoryList;
   private JPanel rootPanel;
 }
